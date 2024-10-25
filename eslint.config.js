9ba2395bf204edcc122543b51bdffdd24a3fb46f
ouyangchengjun <ouyangchengjun@talkweb.com.cn>
2024-10-25 17:51:14 +0800

    部署github

--- eslint.config.js
+++ eslint.config.js
@@ -3,32 +3,31 @@
  * @Author: 欧阳承珺
  * @LastEditors: 欧阳承珺
  * @Date: 2024-10-25 10:38:30
- * @LastEditTime: 2024-10-25 16:18:23
+ * @LastEditTime: 2024-10-25 16:31:53
  */
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import autoImportConfig from './.eslintrc-auto-import.json'
 
-
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,vue}']
   },
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
-  autoImportConfig
+  autoImportConfig,
   {
     rules: {
-      'vue/multi-word-component-names': 'off',
-    },
-  },
+      'vue/multi-word-component-names': 'off'
+    }
+  }
 ]
